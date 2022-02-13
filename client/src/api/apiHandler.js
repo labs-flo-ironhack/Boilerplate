27b55ca6f2acf38a99f5e6e1d36bff9731b03c0d
Flow-Fly <44248262+Flow-Fly@users.noreply.github.com>
2022-02-13 11:31:27 +0100

    Update apiHandler.js

--- client/src/api/apiHandler.js
+++ client/src/api/apiHandler.js
@@ -5,6 +5,12 @@ const service = axios.create({
 	withCredentials: true,
 });
 
+service.interceptors.request.use((config) => {
+	const token = localStorage.getItem("authToken");
+	config.headers.Authorization = token ? `Bearer ${token}` : "";
+	return config;
+})
+
 //! Error handling to use in the catch
 function errorHandler(error) {
 	if (error.response.data) {
